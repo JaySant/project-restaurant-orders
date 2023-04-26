a92b7a36fba290cc1fde5d6ebc363b5f57b44b38
JaySant <jaysant15@gmail.com>
2023-04-26 17:41:27 -0300

    requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,24 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (Ingredient, Restriction)  # noqa: F401, E261, E501
 
 
 # Req 1*
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient('queijo mussarela')
+    assert ingredient1.name == 'queijo mussarela'
+    assert ingredient1.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+        }
+
+    #  __repr__
+    assert repr(ingredient1) == "Ingredient('queijo mussarela')"
+
+    #  __eq__
+    ingredient2 = Ingredient('queijo mussarela')
+    ingredient3 = Ingredient('presunto')
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
+
+    # __hash__
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
